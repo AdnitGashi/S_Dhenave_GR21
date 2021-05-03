e0a9335e3efc7ef0d1ce89bb5a2b7b03e9aa451d
AdnitGashi <66799426+AdnitGashi@users.noreply.github.com>
2021-05-03 17:17:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _netTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strList = new List<string>();
+            
+            strList.Add("test");
+            strList.Add("vv");
+            strList.Add("iksetrash");
+
+            foreach(var str in strList){
+                 Console.WriteLine(str);
+            }
+
+           
+
+        }
+    }
+}
