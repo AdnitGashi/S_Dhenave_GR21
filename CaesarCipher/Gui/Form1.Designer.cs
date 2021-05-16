38d408dd427dece24b2d4ac02551b57128fa1c48
Arben <arben.dedaj1@studen.uni-pr.edu>
2021-05-16 23:45:55 +0200

    Permirsime

--- CaesarCipher/Gui/Form1.Designer.cs
+++ CaesarCipher/Gui/Form1.Designer.cs
@@ -45,72 +45,78 @@
             // 
             // plainText
             // 
-            this.plainText.Location = new System.Drawing.Point(36, 103);
+            this.plainText.Location = new System.Drawing.Point(40, 129);
+            this.plainText.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.plainText.Name = "plainText";
-            this.plainText.Size = new System.Drawing.Size(149, 22);
+            this.plainText.Size = new System.Drawing.Size(167, 26);
             this.plainText.TabIndex = 1;
             // 
             // Key
             // 
-            this.Key.Location = new System.Drawing.Point(36, 154);
+            this.Key.Location = new System.Drawing.Point(40, 192);
+            this.Key.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Key.Name = "Key";
-            this.Key.Size = new System.Drawing.Size(149, 22);
+            this.Key.Size = new System.Drawing.Size(167, 26);
             this.Key.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 71);
+            this.label1.Location = new System.Drawing.Point(37, 89);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.Size = new System.Drawing.Size(73, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Plain text";
             // 
             // cipherText
             // 
-            this.cipherText.Location = new System.Drawing.Point(595, 130);
+            this.cipherText.Location = new System.Drawing.Point(669, 162);
+            this.cipherText.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cipherText.Name = "cipherText";
-            this.cipherText.Size = new System.Drawing.Size(149, 22);
+            this.cipherText.Size = new System.Drawing.Size(167, 26);
             this.cipherText.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(33, 133);
+            this.label2.Location = new System.Drawing.Point(37, 166);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.Size = new System.Drawing.Size(35, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "Key";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(592, 103);
+            this.label3.Location = new System.Drawing.Point(666, 129);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.Size = new System.Drawing.Size(85, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Cipher text";
             // 
             // cipherText2
             // 
-            this.cipherText2.Location = new System.Drawing.Point(595, 291);
+            this.cipherText2.Location = new System.Drawing.Point(669, 364);
+            this.cipherText2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cipherText2.Name = "cipherText2";
-            this.cipherText2.Size = new System.Drawing.Size(149, 22);
+            this.cipherText2.Size = new System.Drawing.Size(167, 26);
             this.cipherText2.TabIndex = 7;
             // 
             // plainText2
             // 
-            this.plainText2.Location = new System.Drawing.Point(36, 291);
+            this.plainText2.Location = new System.Drawing.Point(40, 364);
+            this.plainText2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.plainText2.Name = "plainText2";
-            this.plainText2.Size = new System.Drawing.Size(149, 22);
+            this.plainText2.Size = new System.Drawing.Size(167, 26);
             this.plainText2.TabIndex = 8;
             // 
             // decryptBtn
             // 
             this.decryptBtn.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.decryptBtn.Location = new System.Drawing.Point(310, 275);
+            this.decryptBtn.Location = new System.Drawing.Point(349, 344);
+            this.decryptBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.decryptBtn.Name = "decryptBtn";
-            this.decryptBtn.Size = new System.Drawing.Size(149, 54);
+            this.decryptBtn.Size = new System.Drawing.Size(168, 68);
             this.decryptBtn.TabIndex = 9;
             this.decryptBtn.Text = "Decrypt";
             this.decryptBtn.UseVisualStyleBackColor = false;
@@ -119,27 +125,28 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(592, 261);
+            this.label4.Location = new System.Drawing.Point(666, 326);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 17);
+            this.label4.Size = new System.Drawing.Size(85, 20);
             this.label4.TabIndex = 10;
             this.label4.Text = "Cipher text";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(33, 261);
+            this.label5.Location = new System.Drawing.Point(37, 326);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 17);
+            this.label5.Size = new System.Drawing.Size(73, 20);
             this.label5.TabIndex = 11;
             this.label5.Text = "Plain text";
             // 
             // encryptBtn
             // 
             this.encryptBtn.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.encryptBtn.Location = new System.Drawing.Point(310, 130);
+            this.encryptBtn.Location = new System.Drawing.Point(349, 162);
+            this.encryptBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.encryptBtn.Name = "encryptBtn";
-            this.encryptBtn.Size = new System.Drawing.Size(149, 54);
+            this.encryptBtn.Size = new System.Drawing.Size(168, 68);
             this.encryptBtn.TabIndex = 12;
             this.encryptBtn.Text = "Encrypt";
             this.encryptBtn.UseVisualStyleBackColor = false;
@@ -147,9 +154,10 @@
             // 
             // generateKey
             // 
-            this.generateKey.Location = new System.Drawing.Point(65, 193);
+            this.generateKey.Location = new System.Drawing.Point(73, 241);
+            this.generateKey.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.generateKey.Name = "generateKey";
-            this.generateKey.Size = new System.Drawing.Size(99, 42);
+            this.generateKey.Size = new System.Drawing.Size(111, 52);
             this.generateKey.TabIndex = 13;
             this.generateKey.Text = "Generate Key";
             this.generateKey.UseVisualStyleBackColor = true;
@@ -157,10 +165,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(846, 471);
+            this.ClientSize = new System.Drawing.Size(931, 589);
             this.Controls.Add(this.generateKey);
             this.Controls.Add(this.encryptBtn);
             this.Controls.Add(this.label5);
@@ -174,6 +182,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Key);
             this.Controls.Add(this.plainText);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
